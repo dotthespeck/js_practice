826b7e2534d17203a9a66d38d0ec3c6c392a4f2a
Zoe <doticus@gmail.com>
2015-02-17 14:42:52 -0500

    Sum of array

---
+++ eloquentjs_sumofnumbers.js
@@ -0,0 +1,17 @@
+function range(x, y) {
+  results = [];
+
+  for (i = x; i <= y; i++) {
+    results.push(i);
+  }
+  return results;
+}
+
+function sum(range) {
+  var count=0;
+  for (var i = 0; i < results.length; i++) {
+    count += results[i];
+  }
+
+  return count;
+}
